6a147f31b6f913ac0418452543b8174f7f22d21c
yosiib2 <yosefasefa88@gmail.com>
2025-03-23 22:39:10 +0300

    firstt commit

---
+++ src/main/java/com/mycompany/namee/Namee.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.namee;
+
+/**
+ *
+ * @author yosef
+ */
+public class Namee {
+
+    public static void main(String[] args) {
+        System.out.println("my name is kan");
+        System.out.println("his name is lucho : ");
+        
+    }
+}
